2649ecacf7555c10b0514d021f553b1a34bdde62
Tejas013 <tejasmadhukar6@gmail.com>
2024-06-14 20:40:18 +0530

    testttttt

--- test_ocr/main.go
+++ test_ocr/main.go
@@ -1,19 +1,38 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 
+	"github.com/fatih/color"
 	"github.com/otiai10/gosseract/v2"
 )
 
 func main() {
+	var bookText strings.Builder
 	client := gosseract.NewClient()
+	defer client.Close()
 	client.Languages = []string{"ara"}
-	client.SetImage(".png")
+	client.SetImage("image-0100.jpg")
 	text, err := client.Text()
 	if err != nil {
 		panic(err)
 	}
+
+	file, err := os.Create("test.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	w := bufio.NewWriter(file)
+	w.WriteString(bookText.String())
+	if err := w.Flush(); err != nil {
+		panic(err)
+	}
+
+	color.Green(fmt.Sprintf("Completed: "))
 	fmt.Println(text)
-	client.Close()
 }
