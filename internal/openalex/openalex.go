7c017c56bb4acaa488298dc77c775dc3c72404e7
Tejas013 <tejasmadhukar6@gmail.com>
2024-06-23 17:21:17 +0530

    Made types for openalex object

---
+++ internal/openalex/openalex.go
@@ -0,0 +1,27 @@
+package openalex
+
+type WorkObject struct {
+	OpenAccess      OpenAccess `json:"open_access"`
+	CitedByCount    uint32     `json:"cited_by_count"`
+	Title           string     `json:"title"`
+	PrimaryTopic    Topic      `json:"primary_topic"`
+	Language        string     `json:"language"`
+	PublicationYear uint32     `json:"publication_year"`
+	PublicationDate string     `json:"publication_date"`
+	Text            string     `json:"text"`
+}
+
+type OpenAccess struct {
+	IsOA     bool   `json:"is_oa"`
+	OAURL    string `json:"oa_url"`
+	OAStatus string `json:"oa_status"`
+}
+
+type Topic struct {
+	ID     string `json:"id"`
+	Domain Domain `json:"domain"`
+}
+
+type Domain struct {
+	DisplayName string `json:"display_name"`
+}
