27ce5ed9578ad314e6a24cbc10e3276496e8d929
Tejas013 <tejasmadhukar6@gmail.com>
2024-06-23 17:21:38 +0530

    Made logic to save pdf

---
+++ internal/openalex/download.go
@@ -0,0 +1,49 @@
+package openalex
+
+import (
+	"crypto/tls"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/fatih/color"
+)
+
+var (
+	tr = &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client = &http.Client{
+		Transport: tr,
+	}
+)
+
+func (obj *WorkObject) SaveArticle(downloadDir string) error {
+	resp, err := client.Get(obj.OpenAccess.OAURL)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.Header["Content-Type"][0] != "application/pdf" {
+		return err
+	}
+
+	title := fmt.Sprintf("%s.%s", strings.Split(obj.Title, "/")[3], "pdf")
+	file, err := os.Create(filepath.Join(downloadDir, title))
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	color.Green(fmt.Sprintf("Downloaded: %s", title))
+	return nil
+}
